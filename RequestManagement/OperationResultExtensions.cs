aaf4ee80f6006522519423b957e658903d77a069
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-09-30 20:27:26 +1000

    initial commit

---
+++ RequestManagement/OperationResultExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestManagement
+{
+    /// <summary>
+    /// Operational Result Extension Methods
+    /// </summary>
+    public static class OperationResultExtensions
+    {
+        /// <summary>
+        /// Converts to a <see cref="ValidationProblemDetails"/> object
+        /// </summary>
+        /// <param name="result">Operation result to convert</param>
+        /// <returns>Validation problem details</returns>
+        public static ValidationProblemDetails ToProblemDetails(this OperationResult result)
+        {
+            if (result == null) throw new ArgumentNullException(nameof(result));
+
+            var problemDetails = new ValidationProblemDetails()
+            {
+                Status = (int)HttpStatusCode.BadRequest
+            };
+
+            if (problemDetails.Errors != null)
+            {
+                result.Errors
+                   .ToList()
+                   .ForEach(i => problemDetails.Errors.Add(i.Key, i.Value.ToArray()));
+            }
+
+            return problemDetails;
+        }
+    }
+}
