a2c638e5b9ce7eac16d2e1c6b391cc634825de76
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-10-01 15:51:24 +1000

    database migration

---
+++ SampleApiWebApp/Migrations/20181001054830_InitialMigration.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApiWebApp.Migrations
+{
+    /// <summary>
+    /// Initial Migration
+    /// </summary>
+    public partial class InitialMigration : Migration
+    {
+        /// <summary>
+        /// Builds the operations that will migrate the database 'up'
+        /// </summary>
+        /// <param name="migrationBuilder">Migration builder</param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GivenName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    TeamId = table.Column<long>(nullable: false),
+                    SquadNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TeamId",
+                table: "Players",
+                column: "TeamId");
+        }
+
+        /// <summary>
+        /// Builds the operations that will migrate the database 'down'
+        /// </summary>
+        /// <param name="migrationBuilder">Migration builder</param>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
