fabd392cf4901b31033eb9ff8d56c50d25819ce6
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-10-01 15:05:51 +1000

    player and team mappings

---
+++ SampleApiWebApp/Models/Mappings/PlayerMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace SampleApiWebApp.Models.Mappings
+{
+    /// <summary>
+    /// Player Model Mappings
+    /// </summary>
+    public sealed class PlayerMappings : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerMappings"/> class
+        /// </summary>
+        public PlayerMappings()
+        {
+            CreateMap<Domain.Player, Player>();
+            CreateMap<CreatePlayerRequest, Domain.Player>();
+        }
+    }
+}
---
+++ SampleApiWebApp/Models/Mappings/TeamMappings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace SampleApiWebApp.Models.Mappings
+{
+    /// <summary>
+    /// Team Mappings
+    /// </summary>
+    public sealed class TeamMappings : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamMappings"/> class
+        /// </summary>
+        public TeamMappings()
+        {
+            CreateMap<Domain.Team, Team>();
+            CreateMap<CreateTeamRequest, Domain.Team>();
+        }
+    }
+}
