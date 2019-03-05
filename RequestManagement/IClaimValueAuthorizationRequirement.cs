13eeb4f29d30876a141d676c5b87dbd38fec8a92
Saji Weerasingham <saji.weerasingham@outlook.com>
2019-03-06 04:56:13 +1000

    added claim value authorization handler that extends base claim type authorization handler

---
+++ RequestManagement/IClaimValueAuthorizationRequirement.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestManagement
+{
+    /// <summary>
+    /// Claim Value Authorization Requirement Interface
+    /// </summary>
+    public interface IClaimValueAuthorizationRequirement : IClaimTypeAuthorizationRequirement
+    {
+        /// <summary>
+        /// Gets the claim value
+        /// </summary>
+        string ClaimValue { get; }
+    }
+}
